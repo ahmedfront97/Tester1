f062b3754b8e0f3f6c78fd2a48a191390f3b2226
unknown <letxgtd97@gmail.com>
2024-08-29 10:06:56 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let iconBar = document.getElementById('icon-bar');
+let navBar = document.getElementById('nav-bar');
+
+
+iconBar.onclick = function(){
+    navBar.classList.toggle('show-nav');
+}
+
