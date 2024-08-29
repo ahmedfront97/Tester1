ce30c3e2653b6691a1c27e4fff2bf1f3949ac04c
unknown <letxgtd97@gmail.com>
2024-08-29 15:07:28 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,23 @@ let iconBar = document.getElementById('icon-bar');
 let navBar = document.getElementById('nav-bar');
 
 
+
+
+let slides = document.querySelectorAll('.slide');
+
 iconBar.onclick = function(){
     navBar.classList.toggle('show-nav');
 }
 
+
+let counter = 1;
+function slideShow(par){
+    for(let i = 0 ; i < slides.length ; i++){
+        slides[i].style.opacity='0';
+    }
+    if(counter > slides.length){counter = 1}
+    if(counter < 1){counter = slides.length}
+    slides[counter - 1].style.opacity = '1';
+    
+}
+slideShow(counter);
